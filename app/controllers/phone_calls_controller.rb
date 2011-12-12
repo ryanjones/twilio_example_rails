2cf8f0a64d37bb1114719020c9ec32be28c4d9e4
Ryan Jones <ryan.michael.jones@gmail.com>
2011-12-11 21:58:21 -0700

    removed debugger test

--- app/controllers/phone_calls_controller.rb
+++ app/controllers/phone_calls_controller.rb
@@ -1,7 +1,6 @@
 class PhoneCallsController < ApplicationController
   
   def new
-    debugger
     if params[:client]
       # Load the client's name or whatever from the and put it as the client name
       @phone_call = PhoneCall.new({:client_name => params[:client]})
